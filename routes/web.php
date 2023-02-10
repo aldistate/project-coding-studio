adc8624e9a083aa7ebe0216360387ee2120d5691
aldistate <aldistate@yahoo.com>
2023-02-10 09:47:44 +0700

    15. Auth Middleware

--- routes/web.php
+++ routes/web.php
@@ -40,26 +40,29 @@ Route::get('/', [StudentController::class, 'index'])->name('index');
 // menampilkan data yang sudah di filter di controller
 Route::get('/filter', [StudentController::class, 'filter']);
 
-// menambahkan data ke database
-Route::get('/create', [StudentController::class, 'create'])->name('create');
-
-// mengirim data ke dalam database
-Route::post('/create', [StudentController::class, 'store'])->name('store');
-
-// mendapatkan data inputan lama dari database
-Route::get('/edit/{student}', [StudentController::class, 'edit'])->name('edit');
-
-// mengupdate data ke dalam database
-Route::patch('/update/{student}', [StudentController::class, 'update'])->name('update');
-
-// menghapus data
-Route::delete('/delete/{student}', [StudentController::class, 'delete'])->name('delete');
-
 // update password user
 Route::get('/update_password', [HomeController::class, 'update_password'])->name('update_password');
 
 // mengirim data password user yang mau diubah
 Route::patch('/store_password', [HomeController::class, 'store_password'])->name('store_password');
+
+// middleware untuk admin
+Route::middleware(['admin'])->group(function() {
+  // menambahkan data ke database
+  Route::get('/create', [StudentController::class, 'create'])->name('create');
+
+  // mengirim data ke dalam database
+  Route::post('/create', [StudentController::class, 'store'])->name('store');
+
+  // mendapatkan data inputan lama dari database
+  Route::get('/edit/{student}', [StudentController::class, 'edit'])->name('edit');
+
+  // mengupdate data ke dalam database
+  Route::patch('/update/{student}', [StudentController::class, 'update'])->name('update');
+
+  // menghapus data
+  Route::delete('/delete/{student}', [StudentController::class, 'delete'])->name('delete');
+});
 
 Auth::routes();
 
